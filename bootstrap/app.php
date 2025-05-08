9c2673ebc808d98352fc115c03dc0472866040ae
David KONATE <davidkonate@hotmail.com>
2025-05-08 09:20:01 +0000

    Fix Platform.sh configuration issues

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,13 +52,4 @@ $app->singleton(
 |
 */
 
-$app = new Illuminate\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
-
-// Ajoutez ces lignes
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
-    $_SERVER['HTTPS'] = 'on';
-}
-
 return $app;
